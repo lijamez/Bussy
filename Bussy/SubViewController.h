046622a1aa2f8f18753249c75095faa5d8f432bf
James Li <lijamez@gmail.com>
2011-07-03 00:22:54 -0700

    Shiny icons

---
+++ Bussy/SubViewController.h
@@ -0,0 +1,16 @@
+//
+//  SubViewController.h
+//  Bussy
+//
+//  Created by James Li on 11-07-01.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SubViewController : UIViewController {
+    
+}
+
+@end
